15f46a01f9e0ee3a242012a3eae33e20e820a023
ahmedomer90 <ahmedomer030390@gmail.com>
2026-07-09 03:03:06 +0100

    Add LOH classification script
    Added the second R script to classify ASCAT copy-number fragments as LOH or non-LOH based on nMinor == 0.

---
+++ Projects/MSc-Cancer-Dissertation/scripts/02_classify_LOH.R
@@ -0,0 +1,36 @@
+# 02_classify_LOH.R
+# MSc Cancer Dissertation reconstruction
+# Purpose: classify ASCAT copy-number fragments as LOH or non-LOH
+
+# Load packages
+library(tidyverse)
+
+# Import processed ASCAT segment data
+tracerx_ascat_seg <- readRDS("data/processed/tracerx_ascat_seg_imported.rds")
+
+# Classify fragments as LOH or non-LOH
+# LOH is defined as nMinor == 0
+tracerx_ascat_seg <- tracerx_ascat_seg %>%
+  mutate(
+    LOH = nMinor == 0
+  )
+
+# Check LOH classification
+table(tracerx_ascat_seg$LOH)
+
+# Create separate dataframes
+loh_segments <- tracerx_ascat_seg %>%
+  filter(LOH == TRUE)
+
+non_loh_segments <- tracerx_ascat_seg %>%
+  filter(LOH == FALSE)
+
+# Basic checks
+nrow(tracerx_ascat_seg)
+nrow(loh_segments)
+nrow(non_loh_segments)
+
+# Save outputs
+saveRDS(tracerx_ascat_seg, "data/processed/tracerx_ascat_seg_LOH_classified.rds")
+saveRDS(loh_segments, "data/processed/loh_segments.rds")
+saveRDS(non_loh_segments, "data/processed/non_loh_segments.rds")
